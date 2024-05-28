d050dff99a4375e3b1da3ed6b9737482715c4f17
Thiago Andrade <tao.santos139@gmail.com>
2024-05-27 23:10:10 -0300

    projeto pronto e com comentários

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//Criando os objects que serão usados na class
 const tipoHeroi = {
       guerreiro: "Guerreiro",
       mago: "Mago",
@@ -10,7 +12,8 @@ const tipoAtaque = {
       flechada: "Flechada"
 }
 
-
+//Criando a class do heroi, com os parâmetros solicitados e o uso do this (conoforme sintaxe),
+//em seguida a function atacar que dentro da class não precisa de declaração.
 class heroi {
       constructor(nome, idade, tipo, ataque) {
 
@@ -25,6 +28,7 @@ class heroi {
       }
 }
 
+//Criação dos "jogadores" que usarão a class heroi, os atríbutos criados e a function.
 let player1 = new heroi ("Garoti", 34, "guerreiro", "espada")
 let player2 = new heroi ("Underground", 16, "arqueiro", "flechada")
 
